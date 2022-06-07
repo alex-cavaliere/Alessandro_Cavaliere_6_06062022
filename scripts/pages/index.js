1b0df8365138c3d46c0ad6d7c178e2f7a19b7075
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2022-06-07 15:35:29 +0200

    first commit

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,28 +2,73 @@
         // Penser à remplacer par les données récupérées dans le json
         const photographers = [
             {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
+                "name": "Mimi Keel",
+                "id": 243,
+                "city": "London",
+                "country": "UK",
+                "tagline": "Voir le beau dans le quotidien",
                 "price": 400,
-                "portrait": "account.png"
+                "portrait": "Mimi/Portrait_Nora.jpg"
             },
             {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
+                "name": "Ellie-Rose Wilkens",
+                "id": 930,
+                "city": "Paris",
+                "country": "France",
+                "tagline": "Capturer des compositions complexes",
+                "price": 250,
+                "portrait": "Ellie\ Rose/Architecture_Horseshoe.jpg"
             },
+            {
+                "name": "Tracy Galindo",
+                "id": 82,
+                "city": "Montreal",
+                "country": "Canada",
+                "tagline": "Photographe freelance",
+                "price": 500,
+                "portrait": "Tracy/Fashion_Urban_Jungle.jpg"
+            },
+            {
+                "name": "Nabeel Bradford",
+                "id": 527,
+                "city": "Mexico City",
+                "country": "Mexico",
+                "tagline": "Toujours aller de l'avant",
+                "price": 350,
+                "portrait": "Nabeel/Travel_Outdoor_Baths.jpg"
+            },
+            {
+                "name": "Rhode Dubois",
+                "id": 925,
+                "city": "Barcelona",
+                "country": "Spain",
+                "tagline": "Je crée des souvenirs",
+                "price": 275,
+                "portrait": "Rhode/Fashion_Melody_Red_on_Stripes.jpg"
+            },
+            {
+                "name": "Marcel Nikolic",
+                "id": 195,
+                "city": "Berlin",
+                "country": "Germany",
+                "tagline": "Toujours à la recherche de LA photo",
+                "price": 300,
+                "portrait": "Marcel/Travel_Tower.jpg"
+            }
         ]
         // et bien retourner le tableau photographers seulement une fois
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: [...photographers]})
     }
+
+    /*fetch("./data/photographers.json")
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            console.log(data.photographers);
+        })
+        */
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
