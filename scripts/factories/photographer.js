9c665e243ec754e24c0b5e7e36bca897e023a528
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2022-06-11 03:36:04 +0200

    fetch Json

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,32 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    const { name, id, city, country, tagline, price, portrait } = data;
     const picture = `assets/photographers/${portrait}`;
+    console.log(data);
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        h2.classList.add('name');
         h2.textContent = name;
+        const ville = document.createElement('p');
+        ville.classList.add('city');
+        ville.textContent = city;
+        const region = document.createElement('p');
+        region.classList.add('country');
+        region.textContent = country;
+        const slogan = document.createElement('p');
+        slogan.classList.add('tagline');
+        slogan.textContent = tagline;
+        const prix = document.createElement('p');
+        prix.textContent = price;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(ville);
+        article.appendChild(region);
+        article.appendChild(slogan);
+        article.appendChild(prix);
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,76 +1,28 @@
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Mimi Keel",
-                "id": 243,
-                "city": "London",
-                "country": "UK",
-                "tagline": "Voir le beau dans le quotidien",
-                "price": 400,
-                "portrait": "Mimi/Portrait_Nora.jpg"
-            },
-            {
-                "name": "Ellie-Rose Wilkens",
-                "id": 930,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Capturer des compositions complexes",
-                "price": 250,
-                "portrait": "Ellie\ Rose/Architecture_Horseshoe.jpg"
-            },
-            {
-                "name": "Tracy Galindo",
-                "id": 82,
-                "city": "Montreal",
-                "country": "Canada",
-                "tagline": "Photographe freelance",
-                "price": 500,
-                "portrait": "Tracy/Fashion_Urban_Jungle.jpg"
-            },
-            {
-                "name": "Nabeel Bradford",
-                "id": 527,
-                "city": "Mexico City",
-                "country": "Mexico",
-                "tagline": "Toujours aller de l'avant",
-                "price": 350,
-                "portrait": "Nabeel/Travel_Outdoor_Baths.jpg"
-            },
-            {
-                "name": "Rhode Dubois",
-                "id": 925,
-                "city": "Barcelona",
-                "country": "Spain",
-                "tagline": "Je crée des souvenirs",
-                "price": 275,
-                "portrait": "Rhode/Fashion_Melody_Red_on_Stripes.jpg"
-            },
-            {
-                "name": "Marcel Nikolic",
-                "id": 195,
-                "city": "Berlin",
-                "country": "Germany",
-                "tagline": "Toujours à la recherche de LA photo",
-                "price": 300,
-                "portrait": "Marcel/Travel_Tower.jpg"
-            }
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers]})
-    }
-
-    /*fetch("./data/photographers.json")
+        fetch("./data/photographers.json")
         .then(function(res){
-            return res.json();
+            if(res.ok){
+                return res.json();
+            }
         })
         .then(function(data){
-            console.log(data.photographers);
+            const photographers = data.photographers;
+            console.log(photographers);
+            const photographersSection = document.querySelector(".photographer_section");
+            photographers.forEach((photographer) => {
+                const photographerModel = photographerFactory(photographer);
+                const userCardDOM = photographerModel.getUserCardDOM();
+                photographersSection.appendChild(userCardDOM);
+            });
+        })  
+        .catch(function(err){
+            console.log(err + "error");
         })
-        */
-
-    async function displayData(photographers) {
+        // ho aggiunto fetch alla funzione e ho creato un ciclo for per percorrere gli elementi. 
+        // et bien retourner le tableau photographers seulement une fois
+    }    
+    /*async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
         photographers.forEach((photographer) => {
@@ -78,13 +30,13 @@
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
         });
-    };
-
+    };*/
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
+        const {photographers} = await getPhotographers();
         displayData(photographers);
     };
     
     init();
+
     
